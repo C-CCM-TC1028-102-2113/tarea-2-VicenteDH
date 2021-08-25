426a9d4c8ae84161e4f365a622f9e7f27cf40ea2
VicenteDH <89059919+VicenteDH@users.noreply.github.com>
2021-08-25 17:49:35 -0500

    Update exercise.py

--- assignments/05Ordena/src/exercise.py
+++ assignments/05Ordena/src/exercise.py
@@ -1,5 +1,31 @@
 def main():
-    # Escribe el código adecuado para completar el programa
+x = int(input("Ingresa el primer número: "))
+y = int(input("Ingresa el segundo número: "))
+z = int(input("Ingresa el tercer número: "))
+if x>y>z:
+    print(x)
+    print(y)
+    print(z)
+elif x>z>y:
+    print(x)
+    print(z)
+    print(y)
+elif y>x>z:
+    print(y)
+    print(x)
+    print(z)
+elif y>z>x:
+    print(y)
+    print(z)
+    print(x)
+elif z>x>y:
+    print(z)
+    print(x)
+    print(y)
+elif z>y>x:
+    print(z)
+    print(y)
+    print(x)
     pass
 
 
